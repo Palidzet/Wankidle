df33837ccc3717843d5d11d29195ea2eb87b9800
Arthur Gallier <arthur.gallier@epita.fr>
2024-05-19 19:09:15 +0200

    more parameters to the database

--- Wankidle/main/models.py
+++ Wankidle/main/models.py
@@ -6,7 +6,7 @@ class Database(models.Model):
     
     irl_face_none = models.IntegerField(default=0) #0 pour aucun, 1 pour face cam , 2 pour irl
     game_category = models.CharField(max_length=200, default=' ')
-    game_or_not = models.BooleanField
+    game_or_not = models.BooleanField(default = True)
     feat_or_not = models.CharField(max_length=200, default=' ')
     views = models.IntegerField(default=0)
-    image = models.ImageField
+    image = models.ImageField(blank = True)
